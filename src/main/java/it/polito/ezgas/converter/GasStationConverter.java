c2f7439a230236e41b3eedc4ef4a393f23070b4a
Iman Ostovar <iman.ostovar@studenti.polito.it>
2020-05-12 20:24:31 +0200

    GasStationConverte

--- src/main/java/it/polito/ezgas/converter/GasStationConverter.java
+++ src/main/java/it/polito/ezgas/converter/GasStationConverter.java
@@ -11,8 +11,27 @@ public class GasStationConverter {
 	}
 
 	public static GasStation toGasStation(GasStationDto gasStationDto) {
-		// TODO Auto-generated method stub
-		return null;
+		GasStation gasStation = new GasStation();
+		gasStation.setHasMethane(gasStationDto.getHasMethane());
+		gasStation.setCarSharing(gasStationDto.getCarSharing());
+		gasStation.setLat(gasStationDto.getLat());
+		gasStation.setGasStationId(gasStationDto.getGasStationId());
+		gasStation.setGasStationName(gasStationDto.getGasStationName());
+		gasStation.setGasStationAddress(gasStationDto.getGasStationAddress());
+		gasStation.setHasDiesel(gasStationDto.getHasDiesel());
+		gasStation.setHasSuper(gasStationDto.getHasSuper());
+		gasStation.setLon(gasStationDto.getLon());
+		gasStation.setDieselPrice(gasStationDto.getDieselPrice());
+		gasStation.setSuperPrice(gasStationDto.getSuperPrice());
+		gasStation.setSuperPlusPrice(gasStationDto.getSuperPlusPrice());
+		gasStation.setGasPrice(gasStationDto.getGasPrice());
+		gasStation.setHasSuperPlus(gasStationDto.getHasSuperPlus());
+		gasStation.setUser(UserConverter.toUser(gasStationDto.getUserDto()));
+		gasStation.setReportTimestamp(gasStationDto.getReportTimestamp());
+		gasStation.setReportDependability(gasStationDto.getReportDependability());
+		gasStation.setMethanePrice(gasStationDto.getMethanePrice());
+		gasStation.setReportUser(gasStationDto.getReportUser());
+		return gasStation;
 	}
 
 }
