b877454951c46e8a73fcf4115819a6ff983ce9a2
GIUSEPPE PIPERO s238385 <s238385@studenti.polito.it>
2020-05-30 12:05:07 +0200

    add: GUI tests

---
+++ GUITests/uc2.sikuli/uc2.py
@@ -0,0 +1,11 @@
+click("1590786705590.png")
+wait(1)
+click(Location(663, 244))
+type("a", KeyModifier.CTRL) # select all text
+type(Key.BACKSPACE) # delete selection
+type("newtest")
+type(Location(662, 336), "test")
+click("1590786723933.png")
+wait(1)
+click("1590787763199.png")
+wait("1590787777521.png")
