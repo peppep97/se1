26929540a45fe2b1552021674670a49ffc12bd61
GIUSEPPE PIPERO s238385 <s238385@studenti.polito.it>
2020-06-01 14:21:49 +0200

    add: test controller

--- docs/GUITestReport.md
+++ docs/GUITestReport.md
@@ -12,10 +12,6 @@ This part of the document reports about testing at the GUI level. Tests are end 
 
 ## Coverage of Scenarios and FR
 
-```
-<Complete this table (from IntegrationApiTestReport.md) with the column on the right. In the GUI Test column, report the name of the .py  file with the test case you created.>
-```
-
 ### 
 
 | Scenario ID | Functional Requirements covered | GUI Test(s) |
@@ -26,7 +22,7 @@ This part of the document reports about testing at the GUI level. Tests are end 
 | UC5         | FR3 - FR3.1                     | uc5.py      |   
 | UC6         | FR3 - FR3.2                     | uc6.py      |   
 | UC7         | FR5 - FR5.1                     | uc7.py      |   
-| UC8         | FR4 - FR4.1 - FR4.2 - FR4.3 - FR4.4 - FR4.5                             | uc8.py      |   
+| UC8         | FR4 - FR4.1 - FR4.2 - FR4.3 - FR4.4 - FR4.5   | uc8.py      |   
 | UC9         | FR5 - FR5.2                     | uc9.py      |   
 | UC10        | FR5 - FR5.3                     | uc10.py     |   
             
@@ -44,9 +40,18 @@ Tests should cover each function of classes in the Controller package
 
 | class.method name | Functional Requirements covered |REST  API Test(s) | 
 | ----------- | ------------------------------- | ----------- | 
-|  ...           | FRx                             |             |     
-|  ...           | FRy                             |             |             
-| ...         |                                 |             |             
-| ...         |                                 |             |             
-| ...         |                                 |             |             
-| ...         |                                 |             |             
+|  UserController.getUserById  | FR1 - FR1.4    |testGetUser|     
+|  UserController.getAllUsers  | FR1 - FR1.3    |testGetAllUsers|     
+| UserController.saveUser      | FR1 - FR1.1    |testSaveUser|             
+| UserController.deleteUser    | FR1 - FR1.2    |testDeleteUser|             
+| UserController.increaseUserReputation| FR5 - FR5.2             |testIncreaseUserReputation|             
+| UserController.decreaseUserReputation| FR5 - FR5.2             |testDecreaseUserReputation|             
+| UserController.login         | FR2                             |testLogin|      
+| GasStationController.getGasStationById         | FR4                             |testGetGasStation|  
+| GasStationController.getAllGasStations         | FR4                             |testGetAllGasStations|  
+| GasStationController.saveGasStation            | FR3 - FR3.1                     |testSaveGasStation|  
+| GasStationController.deleteGasStation          | FR3 - FR3.2                                |testDeleteGasStation|  
+| GasStationController.getGasStationsByGasolineType      | FR4 - FR4.4                        |testSearchGasStationByGasolineType|  
+| GasStationController.getGasStationsByProximity         | FR4 - FR4.1 - FR4.2                |testSearchGasStationByProximity|  
+| GasStationController.getGasStationsWithCoordinates     | FR4 - FR4.2 - FR4.5                |testGetGasStationsWithCoordinates|  
+| GasStationController.setGasStationReport               | FR5 - FR5.1                        |testSetGasStationReport|  
--- src/test/java/it/polito/ezgas/controllertests/TestController.java
+++ src/test/java/it/polito/ezgas/controllertests/TestController.java
@@ -1,15 +1,25 @@
 package it.polito.ezgas.controllertests;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import it.polito.ezgas.dto.GasStationDto;
+import it.polito.ezgas.dto.IdPw;
 import it.polito.ezgas.dto.UserDto;
+import it.polito.ezgas.entity.GasStation;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.*;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -22,35 +32,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import it.polito.ezgas.entity.User;
 import it.polito.ezgas.repository.UserRepository;
 
+import static com.fasterxml.jackson.databind.deser.std.UntypedObjectDeserializer.Vanilla.std;
 import static org.junit.Assert.assertEquals;
 
-//@RunWith(SpringRunner.class)
-//@DataJpaTest
-
 public class TestController {
-	
-	@Autowired
-	UserRepository userRepo;
-
-	User a = new User("Alice", "Alice", "alice@ezgas.com", 0);
-	User b = new User("Bob", "Bob", "bob@ezgas.com", 0);
-	User c = new User("Charlie", "Charlie", "charlie@ezgas.com", 0);
-
-	Integer aId, bId, cId;
-
-	/*@Before
-	public void setUp() throws Exception{
-		aId = userRepo.save(a).getUserId();
-		bId = userRepo.save(b).getUserId();
-		cId = userRepo.save(c).getUserId();
-	}
-
-	@After
-	public void tearDown() throws Exception{
-		userRepo.delete(a);
-		userRepo.delete(b);
-		userRepo.delete(c);
-	}*/
 
 	@Test
 	public void testGetUser() throws ClientProtocolException, IOException {
@@ -82,26 +67,38 @@ public class TestController {
 	
 	@Test
 	public void testSaveUser() throws ClientProtocolException, IOException {
-		
-		HttpUriRequest request = new HttpGet("http://localhost:8080/user/saveUser");
+
+		User u = new User("test1", "test1", "test1@test.com", 0);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = mapper.writeValueAsString(u);
+
+		HttpPost request = new HttpPost("http://localhost:8080/user/saveUser");
+
+		StringEntity entity = new StringEntity(jsonString);
+		request.setEntity(entity);
+		request.setHeader("Accept", "application/json");
+		request.setHeader("Content-type", "application/json");
+
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
-		
+
 		assert(response.getStatusLine().getStatusCode() == 200);
 	}
 	
 	@Test
 	public void testDeleteUser() throws ClientProtocolException, IOException {
-		
-		HttpUriRequest request = new HttpGet("http://localhost:8080/user/deleteUser");
+
+		HttpDelete request = new HttpDelete("http://localhost:8080/user/deleteUser/113");
+
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
-		
+
 		assert(response.getStatusLine().getStatusCode() == 200);
 	}
 	
 	@Test
 	public void testIncreaseUserReputation() throws ClientProtocolException, IOException {
-		
-		HttpUriRequest request = new HttpGet("http://localhost:8080/user/increaseUserReputation");
+
+		HttpPost request = new HttpPost("http://localhost:8080/user/increaseUserReputation//1");
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
 		
 		assert(response.getStatusLine().getStatusCode() == 200);
@@ -109,10 +106,138 @@ public class TestController {
 	
 	@Test
 	public void testDecreaseUserReputation() throws ClientProtocolException, IOException {
-		
-		HttpUriRequest request = new HttpGet("http://localhost:8080/user/decreaseUserReputation");
+
+		HttpPost request = new HttpPost("http://localhost:8080/user/decreaseUserReputation//1");
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
 		
+		assert(response.getStatusLine().getStatusCode() == 200);
+	}
+
+	@Test
+	public void testLogin() throws ClientProtocolException, IOException {
+
+		IdPw credentials = new IdPw("test@test.com", "test");
+
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = mapper.writeValueAsString(credentials);
+
+		HttpPost request = new HttpPost("http://localhost:8080/user/login");
+
+		StringEntity entity = new StringEntity(jsonString);
+		request.setEntity(entity);
+		request.setHeader("Accept", "application/json");
+		request.setHeader("Content-type", "application/json");
+
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		assert(response.getStatusLine().getStatusCode() == 200);
+	}
+
+	@Test
+	public void testGetGasStation() throws ClientProtocolException, IOException {
+
+		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/getGasStation/166");
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		String jsonFromResponse = EntityUtils.toString(response.getEntity());
+
+		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		GasStationDto gasStationDto = mapper.readValue(jsonFromResponse, GasStationDto.class);
+
+		assert(gasStationDto.getGasStationName().equals("testGas"));
+	}
+
+	@Test
+	public void testGetAllGasStations() throws ClientProtocolException, IOException {
+
+		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/getAllGasStations");
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		String jsonFromResponse = EntityUtils.toString(response.getEntity());
+
+		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		GasStationDto[] gasStationList = mapper.readValue(jsonFromResponse, GasStationDto[].class);
+
+		assert(gasStationList.length == 5);
+	}
+
+	@Test
+	public void testSaveGasStation() throws ClientProtocolException, IOException {
+
+		GasStation g = new GasStation("testGasPOST", "Turin Piemont Italy", true, true, true, true, true, "Enjoy", 45.0677551, 7.6824892, -1, -1, -1, -1, -1, -1, null, 0);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = mapper.writeValueAsString(g);
+
+		HttpPost request = new HttpPost("http://localhost:8080/gasstation/saveGasStation");
+
+		StringEntity entity = new StringEntity(jsonString);
+		request.setEntity(entity);
+		request.setHeader("Accept", "application/json");
+		request.setHeader("Content-type", "application/json");
+
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		assert(response.getStatusLine().getStatusCode() == 200);
+	}
+
+	@Test
+	public void testDeleteGasStation() throws ClientProtocolException, IOException {
+		HttpDelete request = new HttpDelete("http://localhost:8080/gasstation/deleteGasStation/231");
+
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		assert(response.getStatusLine().getStatusCode() == 200);
+	}
+
+	@Test
+	public void testSearchGasStationByGasolineType() throws ClientProtocolException, IOException {
+
+		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/searchGasStationByGasolineType/Diesel");
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		String jsonFromResponse = EntityUtils.toString(response.getEntity());
+
+		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		GasStationDto[] gasStationList = mapper.readValue(jsonFromResponse, GasStationDto[].class);
+
+		assert(gasStationList.length == 3);
+	}
+
+	@Test
+	public void testSearchGasStationByProximity() throws ClientProtocolException, IOException {
+
+		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/searchGasStationByProximity/45.06/7.68/");
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		String jsonFromResponse = EntityUtils.toString(response.getEntity());
+
+		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		GasStationDto[] gasStationList = mapper.readValue(jsonFromResponse, GasStationDto[].class);
+
+		assert(gasStationList.length == 2);
+	}
+
+	@Test
+	public void testGetGasStationsWithCoordinates() throws ClientProtocolException, IOException {
+
+		HttpUriRequest request = new HttpGet("http://localhost:8080/gasstation/getGasStationsWithCoordinates/45.06/7.68/Diesel/Enjoy");
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		String jsonFromResponse = EntityUtils.toString(response.getEntity());
+
+		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		GasStationDto[] gasStationList = mapper.readValue(jsonFromResponse, GasStationDto[].class);
+
+		assert(gasStationList.length == 1);
+	}
+
+	@Test
+	public void testSetGasStationReport() throws ClientProtocolException, IOException {
+
+		HttpPost request = new HttpPost("http://localhost:8080/gasstation/setGasStationReport/198/1/1/1/1/1/1/");
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
 		assert(response.getStatusLine().getStatusCode() == 200);
 	}
 
